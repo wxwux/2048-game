b7288e7e2bfab470d55b98bca1c09b98c4dff226
Vladimir Sabantsev <vladimir.sabantsev@gmail.com>
2020-08-18 21:48:32 +0300

    убрал логи

--- src/core/initer.ts
+++ src/core/initer.ts
@@ -7,22 +7,9 @@ export const createStartCells = () : GameCell[] => {
   const firstCell: GameCell = create({ x: getRandomCoords(), y: getRandomCoords(), value: 4 });
   const secondCell: GameCell = create({ x: getRandomCoords(), y: getRandomCoords(), value: 4 });
 
-  console.log(firstCell.x, firstCell.y);
-  console.log(secondCell.x, secondCell.y);
-  
-
   if (firstCell.x === secondCell.x && firstCell.y === secondCell.y) {
     firstCell.x = firstCell.x === 0 ? 1 : firstCell.x - 1;
-
-    console.log('true');
-    
   }
-
-  console.log(firstCell, secondCell);
-  
-
-  console.log('run');
-  
 
   return [firstCell, secondCell];
 };
