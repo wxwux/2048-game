1921cf8c4d9e006cd67e2ae0e9e2f7625cbb5157
Vladimir Sabantsev <vladimir.sabantsev@gmail.com>
2021-02-07 19:08:02 +0300

    build 1.0

---
+++ docs/precache-manifest.73ef84ace1eec3dfc052900c17454034.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97249703ee6b8ba23301a61177dbd2d0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4701497da577e8591cb0",
+    "url": "./static/css/main.376633e7.chunk.css"
+  },
+  {
+    "revision": "637f156301fa25fa8f11",
+    "url": "./static/js/2.686b23c6.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "./static/js/2.686b23c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4701497da577e8591cb0",
+    "url": "./static/js/main.f7e04e93.chunk.js"
+  },
+  {
+    "revision": "70b1d1add2180584a2c1",
+    "url": "./static/js/runtime-main.d9b13937.js"
+  }
+]);
